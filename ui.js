ffe0e11d108a2fcb07c81059219636bd48fba5e6
Ugo Sangiorgi <ugo.sangiorgi@elastic.co>
2019-06-16 10:14:16 -0500

    ui working

--- ui.js
+++ ui.js
@@ -1,12 +1,18 @@
 var WebSocket = require('ws');
 
-var blessed = require('blessed'),
-    screen;
-var hljs = require('highlight.js');
-var h2c = require('highlight.js-console'); 
+var blessed = require('blessed'), screen;
 
-
+var emphasize = require('emphasize')
+var chalk = require('chalk')
 var messages = [];
+
+const color_config = {
+    'attr': chalk.red,
+    'string': chalk.white,
+    'number': chalk.magenta
+}
+
+const MAX_MESSAGES = 500;
 
 screen = blessed.screen({
     //dump: __dirname + '/log.txt',
@@ -15,7 +21,7 @@ screen = blessed.screen({
     warnings: true
 });
 
-var logger = blessed.list({
+var message_list = blessed.list({
     parent: screen,
     top: 1,
     left: 'left',
@@ -44,24 +50,28 @@ var logger = blessed.list({
 });
 
 var title = blessed.Text({
-    left: 0,
+    left: 1,
     top: 0,
     // border: 'line',
-    content: 'Logstash Debug'
+    content: 'Logstash Debug',
+    style: {
+        inverse: true
+    }
 });
 
+
+
 var ws_connected = blessed.Text({
-    right: 0,
+    right: 1,
     top: 0,
     content: 'Connected: No'
 });
 
-var topright = blessed.box({
-
+var message_viewer = blessed.box({
     left: '20%',
     top: 1,
     width: '80%',
-    height: '99%',
+    height: '90%',
     border: {
         type: 'line',
         left: true,
@@ -69,37 +79,36 @@ var topright = blessed.box({
         right: true,
         bottom: true
     },
-    // border: 'line',
-    content: 'Bar'
-
+    content: ''
 });
 
-/* highlight(JSON.stringify({foo:{bar:{baz:true}}}, null, 2), 'json', 'console', function(data) {
-   topright.content = data;
- });*/
+message_list.focus();
+message_list.select(0);
 
-logger.focus();
-logger.select(0);
+var show_message = function(index) {
+    if(messages.length > 0){
+        var jsonmessage = JSON.parse(messages[index]);
+        message_viewer.content = emphasize.highlightAuto(JSON.stringify(jsonmessage, null, 2), color_config).value;
+    }
+}
 
-logger.on('keypress', function (ch, key) {
+message_list.on('keypress', function (ch, key) {
     if (key.name === 'up' || key.name === 'down') {
-        var hlText = hl2c.highlightAuto(JSON.stringify(messages[logger.getScroll()], null, 2));
-        
-        topright.content = hlText;
-
-        /*highlight(JSON.stringify(content, null, 2), 'json', 'console', function(data) {
-            topright.content = data;
-          });*/
+        show_message(message_list.getScroll());
     }
 
 });
 
-logger.on('select', function () {
-    topright.content = JSON.stringify(messages[logger.getScroll()], null, 2);
-
+message_list.on('select', function () {
+    show_message(message_list.getScroll());
 });
 
-screen.append(topright);
+message_list.on('click', function () {
+    show_message(message_list.getScroll());
+});
+
+
+screen.append(message_viewer);
 screen.append(ws_connected);
 screen.append(title);
 
@@ -128,13 +137,15 @@ ws.on('close', function open() {
 
 ws.on('message', function incoming(message) {
     const jsonmessage = JSON.parse(message);
-    logger.add(jsonmessage["@timestamp"]||"event");
-    messages.push(message);
+    
+    if(messages.length <= MAX_MESSAGES){
+        message_list.add(jsonmessage["@timestamp"] || "event "+message_list.items.length);
+        messages.push(message);
+    }
 });
 
-setInterval(function(){
+setInterval(function () {
     screen.render();
 }, 200);
-
 
 screen.render();
